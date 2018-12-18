8187e849d8e349b9008bcab3bb686f40ba7b4e02
Boulix <maxenceperso@gmail.com>
2018-12-18 11:38:14 +0100

    Poursuite de l'implémentation la fonction minMax

--- minMax.py
+++ minMax.py
@@ -11,13 +11,25 @@ def minMax(board, depth):
     
     if board.turn :
         maxValue = -10**22
-        bestMove = none
+        bestMove = None
         for move in moves :
             nextMove = chess.Move.from_uci(chess.Move.uci(move))
             board.push(nextMove)
             val, bestMove = minMax(board, depth-1)
             board.pop()
-            
+            maxValue = max(maxValue, val)
+        return maxValue
+    else :
+        minValue = 10**22
+        bestMove = None
+        for move in moves :
+            nextMove = chess.Move.from_uci(chess.Move.uci(move))
+            board.push(nextMove)
+            val, bestMove = minMax(board, depth-1)
+            board.pop()
+            minValue = min(minValue, val)
+        return maxValue
+        
         
         
 
